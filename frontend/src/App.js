9dac3637ebb562e9cb3ce9926ce51d5c2bf5058c
Hawaii Ho <hawaiishinji@gmail.com>
2020-01-28 21:04:38 +0800

    use material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import axios from 'axios'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function App() {
 
+  const classes = useStyles();
   const [message, setMessage] = useState('');
   const [link, setLink] = useState('');
   const [apiResultMessage, setApiResultMessage] = useState('');
@@ -24,7 +46,7 @@ function App() {
       setMessage('')
       setLink('')
       const linkUrl = `https://www.facebook.com/${result.data.id}`
-      setApiResultMessage((<div>送出成功 <a href={linkUrl}>FB 文章連結</a></div>))
+      setApiResultMessage((<div>送出成功 <Link href={linkUrl}>FB 文章連結</Link></div>))
     }).catch((err)=>{
       if (err.response) {
         
@@ -39,23 +61,54 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>靠北爛標題</h1>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          靠北爛標題
+        </Typography>
         <p>
           請輸入留言以及爛標題的連結
         </p>
-        留言:
-        <textarea value={message} onChange={handleMessageChange} />
-        連結: (必填)
-        <textarea value={link} onChange={handleLinkChange} />
-
-        <button onClick={handleClick}>
-          送出
-        </button>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            multiline
+            rows="4"
+            fullWidth
+            id="message"
+            label="留言"
+            name="message"
+            size="medium"
+            autoFocus
+            value={message}
+            onChange={handleMessageChange} 
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="link"
+            label="連結"
+            id="link"
+            size="small"
+            value={link}
+            onChange={handleLinkChange}
+          />
+          <Button
+            // type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleClick}
+          >送出</Button>
+        </form>
         {apiResultMessage}
-      </header>
-    </div>
+      </div>
+    </Container>
   );
 }
 
